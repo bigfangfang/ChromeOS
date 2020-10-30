ad8f4494dc6657da315432920aeb7955cd41f3d7
BigFang <44218396+bigfangfang@users.noreply.github.com>
2020-10-30 09:37:03 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# SUBSCRIBE to BigFang on youtube for more such videos 
+sudo apt-get update
+sudo apt-get install figlet
+sudo apt-get install pv
+sudo apt-get install cgpt
+sudo figlet -c "SUBSCRIBE TO"
+sudo figlet -c Big
+sudo figlet -c Fang
+sudo echo https://www.youtube.com/channel/UCMkk-akdGTugIVu10-bXQEQ
+sudo bash chromeos-install.sh -src rammus_recovery.bin -dst /dev/sda
